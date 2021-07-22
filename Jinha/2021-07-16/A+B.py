ee72184dc135cce957c4803cf672aba4ee579dd2
Foliv0ra <56428206+bradypus404@users.noreply.github.com>
2021-07-23 05:04:08 +0900

    Delete Jinha/2021-07-16 directory

--- Jinha/2021-07-16/A+B.py
+++
@@ -1,5 +0,0 @@
-import sys
-tc = int(sys.stdin.readline().rstrip())
-for i in range(tc):
-    a, b = map(int, sys.stdin.readline().rstrip().split())
-    print(a+b)
--- Jinha/2021-07-16/sort.py
+++
@@ -1,8 +0,0 @@
-import sys
-tc = int(sys.stdin.readline())
-sortList = list()
-for i in range(tc):
-    sortList.append(int(sys.stdin.readline()))
-sortList.sort()
-for i in sortList:
-    print(i)
